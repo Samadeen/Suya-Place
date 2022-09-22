af406663b75fc4f82ef10e7510d16c5eb50a9516
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-09-22 20:40:34 +0100

    Populated the Base Component to accept Type

--- src/components/Base.js
+++ src/components/Base.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Base = () => {
-  return <div>Base</div>;
+const Base = ({ addType, suya }) => {
+  const types = ['Classic', 'Aboki Special', 'Dried Hot', 'Soft Hot'];
+  console.log(suya);
+
+  return (
+    <div className='container base flex'>
+      <div>
+        <h2>Step 1: Choose your type</h2>
+        <ul>
+          {types.map((type) => {
+            const addTypeHandler = () => {
+              addType(type);
+            };
+            let list = suya.type === type ? 'active' : '';
+            return (
+              <li key={type} onClick={addTypeHandler}>
+                <span className={list}>{type}</span>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      {suya.type && (
+        <div className='next'>
+          <Link to='/toppings'>
+            <button className='btn btn-next'>Next</button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Base;
