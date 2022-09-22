ca4a3228e836384043e10afb1e53a7996e0efc2b
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-09-22 20:41:14 +0100

    Home Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className='home container flex'>
+      <h2>Welcome to the Suya Place</h2>
+      <Link to='./base'>
+        <button className='btn btn-primary'>Customize Your Suya</button>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
