7cbfafa543bebd3b932f9dcedba3c035a5982759
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-09-22 23:09:17 +0100

    Created the toppings component

--- src/components/Toppings.js
+++ src/components/Toppings.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Toppings = () => {
-  return <div>Toppings</div>;
+const Toppings = ({ addToppings, suya }) => {
+  const toppings = [
+    'Onions',
+    'Cabbage',
+    'Ginger',
+    'Garlic',
+    'Spring onions',
+    'Yagi',
+    'Tomatoes',
+  ];
+
+  return (
+    <div className='toppings container flex'>
+      <div>
+        <h2>What will like as toppings:</h2>
+        {toppings.map((top) => {
+          const addToppingsHandler = () => {
+            addToppings(top);
+          };
+
+          let list = suya.toppings.includes(top) ? 'active' : '';
+          return (
+            <ul key={top} onClick={addToppingsHandler}>
+              <li>
+                <span className={list}>{top}</span>
+              </li>
+            </ul>
+          );
+        })}
+      </div>
+      <Link to='/order'>
+        <button className='btn btn-next'>Order</button>
+      </Link>
+    </div>
+  );
 };
 
 export default Toppings;
