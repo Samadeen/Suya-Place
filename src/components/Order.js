414503618084ba262ad5271dfe164abbe1365dfe
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-09-22 23:09:37 +0100

    Created the Order Component

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,7 +1,15 @@
-import React from 'react';
-
-const Order = () => {
-  return <div>Order</div>;
+const Order = ({ suya }) => {
+  return (
+    <div className='container order flex'>
+      <h2>Thank you for Ordering 😎</h2>
+      <p>You Ordered a "{suya.type} Suya" with:</p>
+      {suya.toppings.map((topping) => (
+        <p key={topping} className='order_para'>
+          {topping}
+        </p>
+      ))}
+    </div>
+  );
 };
 
 export default Order;
