e8baa25a1e998583fc9ac0d68b4a2a635d065528
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-09-22 20:41:01 +0100

    Header Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className='header'>
+      <div className='logo'>
+        <h1>SP</h1>
+      </div>
+      <div className='header_nav'>
+        <h1>Suya Place</h1>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
